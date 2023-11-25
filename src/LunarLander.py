c90fe2cda2cbecd15828cf824e4c595158a13231
Pierre Pinçon <74105302+PinconP@users.noreply.github.com>
2023-11-25 17:28:07 +0100

    Update so now the best model only is kept

--- src/LunarLander.py
+++ src/LunarLander.py
@@ -4,9 +4,25 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import EvalCallback
+
+env_name = "LunarLander-v2"
 
 # Create environment
-env = gym.make('LunarLander-v2')
+env = gym.make(env_name)
+
+# Create the evaluation envs
+eval_envs = make_vec_env(env_name, n_envs=5)
+
+
+# Create evaluation callback to save best model
+# and monitor agent performance
+eval_callback = EvalCallback(
+    eval_envs,
+    best_model_save_path="./models/ppo-LunarLander",
+    eval_freq=1,
+    n_eval_episodes=10,
+)
 
 # Model definition
 model = PPO(
@@ -20,8 +36,8 @@ model = PPO(
     ent_coef=0.01,
     verbose=1)
 
-# Train it for 1,000,000 timesteps
-model.learn(total_timesteps=1000000)
-# Save the model
-model_name = "./models/ppo-LunarLander-v2"
-model.save(model_name)
+# Train the agent (you can kill it before using ctrl+c)
+try:
+    model.learn(total_timesteps=int(5e6), callback=eval_callback)
+except KeyboardInterrupt:
+    pass
---
+++ src/visualizers/LunarLander_visu.py
@@ -0,0 +1,31 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+import cv2  # Pour l'affichage
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.evaluation import evaluate_policy
+
+# Charger l'environnement vectorisé
+env = make_vec_env("LunarLander-v2", n_envs=1)
+
+# Charger le modèle entraîné
+model_name = "models/ppo-LunarLander/best_model"
+model = PPO.load(model_name)
+
+eval_env = Monitor(gym.make("LunarLander-v2"))
+mean_reward, std_reward = evaluate_policy(
+    model, eval_env, n_eval_episodes=10, deterministic=True
+)
+print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+
+# Exécuter l'environnement pour visualiser le modèle
+num_episodes = 5
+for episode in range(num_episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, done, info = env.step(action)
+        env.render(mode="human")
+env.close()
