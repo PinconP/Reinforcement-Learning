6cc4c7265ccd20a19d40be105ddba2a0489ec566
Pierre Pinçon <74105302+PinconP@users.noreply.github.com>
2023-11-27 14:39:11 +0100

    Update ppo_optimizer.py

--- src/PPO/ppo_optimizer.py
+++ src/PPO/ppo_optimizer.py
@@ -1,43 +1,183 @@
-import gymnasium as gym
-from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.callbacks import EvalCallback
+""" Optuna example that optimizes the hyperparameters of
+a reinforcement learning agent using A2C implementation from Stable-Baselines3
+on a Gymnasium environment.
+"""
+
+from typing import Any
+from typing import Dict
+
+import gymnasium
 import optuna
-
-env_name = "LunarLander-v2"  # Name of the environment to be used
-env = make_vec_env(env_name, n_envs=16)
-eval_envs = make_vec_env(env_name, n_envs=5)
-
-
-def objective(trial):
-    # Hyperparameters to tune
-    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2)
-    n_steps = trial.suggest_categorical('n_steps', [256, 512, 1024, 2048])
-    batch_size = trial.suggest_categorical('batch_size', [32, 64, 128, 256])
-    gamma = trial.suggest_float('gamma', 0.9, 0.9999)
-    gae_lambda = trial.suggest_float('gae_lambda', 0.8, 1.0)
-    ent_coef = trial.suggest_float('ent_coef', 0.0001, 0.1)
-    n_epochs = trial.suggest_int('n_epochs', 1, 10)
-
-    # Create a PPO model with these hyperparameters
-    model = PPO('MlpPolicy', env, learning_rate=learning_rate, n_steps=n_steps,
-                batch_size=batch_size, gamma=gamma, gae_lambda=gae_lambda,
-                ent_coef=ent_coef, n_epochs=n_epochs, verbose=0)
-
-    # Train the model for a fixed number of timesteps
-    model.learn(total_timesteps=100000)  # Smaller number for trial runs
-
-    # Evaluate the model
-    mean_reward, _ = evaluate_policy(model, eval_envs, n_eval_episodes=10)
-
-    return mean_reward
+from optuna.pruners import MedianPruner
+from optuna.samplers import TPESampler
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
+import torch
+import torch.nn as nn
 
 
-# Optimization study
-study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=1000)  # Define the number of trials
+N_TRIALS = 1000
+N_STARTUP_TRIALS = 5
+N_EVALUATIONS = 2
+N_TIMESTEPS = int(5e5)
+EVAL_FREQ = int(N_TIMESTEPS / N_EVALUATIONS)
+N_EVAL_EPISODES = 3
 
-# Best hyperparameters
-print("Best hyperparameters:", study.best_params)
+ENV_ID = "LunarLander-v2"
+
+DEFAULT_HYPERPARAMS = {
+    "policy": "MlpPolicy",
+    "env": ENV_ID,
+}
+
+
+# Sample PPO Parameters Function
+def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
+    """Sampler for PPO hyperparameters."""
+    # PPO specific hyperparameters
+    clip_range = trial.suggest_float("clip_range", 0.1, 0.4)
+    n_epochs = trial.suggest_int("n_epochs", 1, 10)
+    batch_size = 2 ** trial.suggest_int("batch_size_exponent", 4, 11)
+    gamma = 1.0 - trial.suggest_float("gamma", 0.0001, 0.1, log=True)
+    max_grad_norm = trial.suggest_float("max_grad_norm", 0.3, 5.0, log=True)
+    gae_lambda = 1.0 - trial.suggest_float("gae_lambda", 0.001, 0.2, log=True)
+    n_steps = 2 ** trial.suggest_int("exponent_n_steps", 3, 10)
+    learning_rate = trial.suggest_float("lr", 1e-5, 1, log=True)
+    ent_coef = trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True)
+    ortho_init = trial.suggest_categorical("ortho_init", [False, True])
+    net_arch = trial.suggest_categorical("net_arch", ["tiny", "small"])
+    activation_fn = trial.suggest_categorical(
+        "activation_fn", ["tanh", "relu"])
+
+    # Display true values.
+    trial.set_user_attr("gamma_", gamma)
+    trial.set_user_attr("gae_lambda_", gae_lambda)
+    trial.set_user_attr("n_steps", n_steps)
+
+    net_arch = {
+        "pi": [64, 64] if net_arch == "small" else [64],
+        "vf": [64, 64] if net_arch == "small" else [64]
+    }
+
+    activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU}[activation_fn]
+
+    return {
+        "n_steps": n_steps,
+        "gamma": gamma,
+        "gae_lambda": gae_lambda,
+        "learning_rate": learning_rate,
+        "ent_coef": ent_coef,
+        "max_grad_norm": max_grad_norm,
+        "policy_kwargs": {
+            "net_arch": net_arch,
+            "activation_fn": activation_fn,
+            "ortho_init": ortho_init,
+
+        },
+        "clip_range": clip_range,
+        "n_epochs": n_epochs,
+        "batch_size": batch_size
+    }
+
+
+class TrialEvalCallback(EvalCallback):
+    """Callback used for evaluating and reporting a trial."""
+
+    def __init__(
+        self,
+        eval_env: gymnasium.Env,
+        trial: optuna.Trial,
+        n_eval_episodes: int = 5,
+        eval_freq: int = 10000,
+        deterministic: bool = True,
+        verbose: int = 0,
+    ):
+        super().__init__(
+            eval_env=eval_env,
+            n_eval_episodes=n_eval_episodes,
+            eval_freq=eval_freq,
+            deterministic=deterministic,
+            verbose=verbose,
+        )
+        self.trial = trial
+        self.eval_idx = 0
+        self.is_pruned = False
+
+    def _on_step(self) -> bool:
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            super()._on_step()
+            self.eval_idx += 1
+            self.trial.report(self.last_mean_reward, self.eval_idx)
+            # Prune trial if need.
+            if self.trial.should_prune():
+                self.is_pruned = True
+                return False
+        return True
+
+
+def objective(trial: optuna.Trial) -> float:
+    kwargs = DEFAULT_HYPERPARAMS.copy()
+    # Sample hyperparameters for PPO.
+    kwargs.update(sample_ppo_params(trial))
+    # Create the RL model using PPO.
+    model = PPO(**kwargs)
+    # Create env used for evaluation.
+    eval_env = Monitor(gymnasium.make(ENV_ID))
+    # Create the callback that will periodically evaluate and report the performance.
+    eval_callback = TrialEvalCallback(
+        eval_env, trial, n_eval_episodes=N_EVAL_EPISODES, eval_freq=EVAL_FREQ, deterministic=True
+    )
+
+    nan_encountered = False
+    try:
+        model.learn(N_TIMESTEPS, callback=eval_callback)
+    except AssertionError as e:
+        # Sometimes, random hyperparams can generate NaN.
+        print(e)
+        nan_encountered = True
+    finally:
+        # Free memory.
+        model.env.close()
+        eval_env.close()
+
+    # Tell the optimizer that the trial failed.
+    if nan_encountered:
+        return float("nan")
+
+    if eval_callback.is_pruned:
+        raise optuna.exceptions.TrialPruned()
+
+    return eval_callback.last_mean_reward
+
+
+if __name__ == "__main__":
+    # Set pytorch num threads to 1 for faster training.
+    torch.set_num_threads(1)
+
+    sampler = TPESampler(n_startup_trials=N_STARTUP_TRIALS)
+    # Do not prune before 1/3 of the max budget is used.
+    pruner = MedianPruner(n_startup_trials=N_STARTUP_TRIALS,
+                          n_warmup_steps=N_EVALUATIONS // 3)
+
+    study = optuna.create_study(
+        sampler=sampler, pruner=pruner, direction="maximize")
+    try:
+        study.optimize(objective, n_trials=N_TRIALS, timeout=600)
+    except KeyboardInterrupt:
+        pass
+
+    print("Number of finished trials: ", len(study.trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
+
+    print("  User attrs:")
+    for key, value in trial.user_attrs.items():
+        print("    {}: {}".format(key, value))
