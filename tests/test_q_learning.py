26ce403d7c838f3d3d406228e8480ba7b638827f
Pierre Pinçon <pierre.pincon@univ-tlse3.fr>
2023-12-11 18:08:09 +0100

    Update test_q_learning.py
    Update tests for better coverage

--- tests/test_q_learning.py
+++ tests/test_q_learning.py
@@ -3,26 +3,30 @@ import gym
 import numpy as np
 from tqdm import tqdm
 import random
-from src.q_learning.q_learning import FrozenLakeAgent
+from src.q_learning.q_learning import FrozenLakeAgent, create_environment
 
 env_id = "FrozenLake-v1"
 
 
 class TestFrozenLakeAgent(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Create a simple, deterministic environment for testing
-        cls.env = gym.make(env_id, is_slippery=False)
-        cls.agent = FrozenLakeAgent(cls.env)
+    def setUp(self):
+        self.env = create_environment()
+        self.agent = FrozenLakeAgent(self.env)
 
-    def test_init(self):
+    def test_environment_creation(self):
+        self.assertIsNotNone(self.env)
+        self.assertGreater(self.env.observation_space.n, 0)
+        self.assertGreater(self.env.action_space.n, 0)
+
+    def test_agent_initialization(self):
         self.assertEqual(
             self.agent.q_table.shape,
             (self.env.observation_space.n, self.env.action_space.n),
         )
-        self.assertTrue(np.all(self.agent.q_table == 0))
+        self.assertGreater(self.agent.learning_rate, 0)
+        self.assertGreater(self.agent.gamma, 0)
 
-    def test_train(self):
+    def test_training_process(self):
         # Create a separate instance of the environment for training
         training_env = gym.make(env_id, is_slippery=False)
         training_agent = FrozenLakeAgent(training_env)
@@ -38,37 +42,30 @@ class TestFrozenLakeAgent(unittest.TestCase):
             max_steps=100,
         )
 
-        # Check if the Q-table has changed from its initial state
-        self.assertFalse(np.array_equal(initial_q_table, training_agent.q_table))
-
-    def test_evaluate(self):
-        mean_reward, _ = self.agent.evaluate(10, 100)
-        self.assertIsInstance(mean_reward, float)
-
     def test_epsilon_greedy_action(self):
-        high_epsilon_action_counts = np.zeros(self.env.action_space.n)
-        low_epsilon_action_counts = np.zeros(self.env.action_space.n)
-        for _ in range(1000):
-            high_epsilon_action = self.agent.epsilon_greedy_action(0, 0.9)
-            low_epsilon_action = self.agent.epsilon_greedy_action(0, 0.1)
-            high_epsilon_action_counts[high_epsilon_action] += 1
-            low_epsilon_action_counts[low_epsilon_action] += 1
-        self.assertTrue(np.any(high_epsilon_action_counts > 250))
-        self.assertTrue(
-            np.argmax(low_epsilon_action_counts) == np.argmax(self.agent.q_table[0])
-        )
-
-    def test_update_q_table(self):
         state = 0
-        action = 1
-        reward = 1
-        new_state = 1
-        self.agent.update_q_table(state, action, reward, new_state)
-        self.assertNotEqual(self.agent.q_table[state, action], 0)
+        action_high_epsilon = self.agent.epsilon_greedy_action(state, 0.9)
+        action_low_epsilon = self.agent.epsilon_greedy_action(state, 0.1)
+        self.assertIn(action_high_epsilon, range(self.env.action_space.n))
+        self.assertIn(action_low_epsilon, range(self.env.action_space.n))
 
-    def test_calculate_epsilon(self):
-        epsilon = self.agent.calculate_epsilon(5, 0.01, 1, 0.1)
-        self.assertTrue(0.01 <= epsilon <= 1)
+    def test_q_table_update(self):
+        state, action, reward, new_state = 0, 1, 1, 1
+        original_value = self.agent.q_table[state, action]
+        self.agent.update_q_table(state, action, reward, new_state)
+        self.assertNotEqual(self.agent.q_table[state, action], original_value)
+
+    def test_epsilon_decay(self):
+        epsilon_initial = FrozenLakeAgent.calculate_epsilon(0, 0.05, 1.0, 0.01)
+        epsilon_final = FrozenLakeAgent.calculate_epsilon(1000, 0.05, 1.0, 0.01)
+        self.assertGreater(epsilon_initial, epsilon_final)
+        self.assertGreaterEqual(epsilon_final, 0.05)
+
+    def test_evaluation_function(self):
+        self.agent.train(10, 1.0, 0.05, 0.001, 100)
+        mean_reward, std_reward = self.agent.evaluate(10, 100)
+        self.assertGreaterEqual(mean_reward, 0)
+        self.assertGreaterEqual(std_reward, 0)
 
 
 if __name__ == "__main__":
